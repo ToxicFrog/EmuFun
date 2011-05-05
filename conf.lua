c9afb370b27315586ae12421995c51c7c7f033bd
Ben Kelly <bk@ancilla.ca>
2011-05-05 11:02:56 -0400

    Documented conf.lua a bit

--- conf.lua
+++ conf.lua
@@ -10,12 +10,12 @@ function love.conf(t)
     t.screen.vsync = true
     t.screen.fsaa = 0
 
-    t.modules.joystick = true
     t.modules.graphics = true
+    t.modules.joystick = true
     t.modules.keyboard = true
-    t.modules.event = true
-    t.modules.timer = true
-    t.modules.mouse = true
+    t.modules.event = true      -- or none of the event handlers work
+    t.modules.timer = true      -- for framerate limiting
+    t.modules.mouse = true      -- for mouse.setVisible
 
     t.modules.audio = false
     t.modules.image = false
