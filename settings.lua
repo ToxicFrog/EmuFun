1a139609afb643232f45e401a79906dcfde8c4cd
Ben Kelly <bk@ancilla.ca>
2011-05-22 10:18:20 -0400

    Leave fullscreen mode when launching games

--- settings.lua
+++ settings.lua
@@ -3,6 +3,7 @@
 -- this function is used to launch a game. This is the standard configuration
 -- for Linux, which assumes config files are actually bash scripts.
 function emufun.launch(gamedir, rom, config)
+    love.graphics.toggleFullscreen()
     os.execute('env GAMEDIR="%s" ROM="%s" CONFIG="%s" bash "%s"' % {
         gamedir,
         rom,
@@ -10,6 +11,7 @@ function emufun.launch(gamedir, rom, config)
         config
     })
     love.event.clear()
+    love.graphics.toggleFullscreen()
 end
 
 -- this is the "old-style" launcher, for when emufun is called by another program
