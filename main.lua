9b5b734b42f4bba716b5d52849abcfb5c4ca6363
Ben Kelly <bkelly@uoguelph.ca>
2011-05-22 21:31:44 -0400

    Support libraries are now loaded with love.filesystem.load rather than require; settings.lua is now copied to the user settings directory

--- main.lua
+++ main.lua
@@ -1,23 +1,33 @@
 emufun = {}
 
+function load(module)
+    return assert(love.filesystem.load(module..".lua"))()
+end
+
 require "lfs"
-require "util"
-require "input"
-require "node"
-require "calibration"
-require "gamelist"
-require "settings"
+
+load "util"
+load "util"
+load "input"
+load "node"
+load "calibration"
+load "gamelist"
 
 function love.load()
-    eprintf("Reading game library:")
+    eprintf("Loading user settings: ")
+    love.filesystem.load("settings.lua")()
+    love.filesystem.write("settings.lua", love.filesystem.read("settings.lua"))
+    eprintf("done.\n")
+    
+    eprintf("Reading game library: ")
     emufun.root = node.new(arg[2] or os.getenv("GAMEDIR") or ".")
     emufun.root:populate()
     for sys in emufun.root:children() do
         sys:populate()
     end
-    eprintf(" done\n")
+    eprintf("done\n")
     
-    eprintf("Setup renderer:")
+    eprintf("Setup renderer: ")
     axes = { hat = love.joystick.getHat(0, 0); love.joystick.getAxes(0) }
     
     --love.graphics.setFont("LiberationMono-Bold.ttf", 24)
@@ -25,8 +35,8 @@ function love.load()
     love.graphics.setBackgroundColor(0, 0, 0)
     love.graphics.setCaption("EmuFun")
     love.mouse.setVisible(false)
+    eprintf("done\n")
     
-    eprintf(" done\n")
     emufun.calibration()
 end
 
