8ec04816ea2dd56861be4c7d079888946928a605
Ben Kelly <bkelly@uoguelph.ca>
2011-09-12 23:06:26 -0400

    io.readn needed for --!emufun support

--- util.lua
+++ util.lua
@@ -9,3 +9,12 @@ getmetatable("").__mod = function(self, arg)
         return self:format(arg)
     end
 end
+
+function io.readn(name, len)
+    local fd = io.open(name, "rb")
+    if not fd then return nil end
+    
+    local buf = fd:read(len)
+    fd:close()
+    return buf
+end
