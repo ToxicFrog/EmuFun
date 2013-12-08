9930f595468d6484204b9f6c9346f281fa073b85
Ben Kelly <bk@ancilla.ca>
2013-12-08 16:49:22 -0500

    Add LOG functions

--- util.lua
+++ util.lua
@@ -34,3 +34,36 @@ function table.copy(from, to)
     
     return to
 end
+
+-- temporary settings structure
+emufun.config = { flags = { log_debug = true } }
+
+local log_levels = { "error", "warning", "info", "debug" }
+
+LOG = {}
+setmetatable(LOG, LOG)
+
+function LOG:__call(level, ...)
+    for i=level,#log_levels do
+        if emufun.config.flags["log_"..log_levels[i]] then
+            print(log_levels[level]:upper(), ...)
+            return
+        end
+    end
+end
+
+function LOG.ERROR(...)
+    return LOG(1, ...)
+end
+
+function LOG.WARNING(...)
+    return LOG(2, ...)
+end
+
+function LOG.INFO(...)
+    return LOG(3, ...)
+end
+
+function LOG.DEBUG(...)
+    return LOG(4, ...)
+end
