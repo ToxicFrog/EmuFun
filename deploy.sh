50312b11132a69e5ee3b2ec1304eaf619e20ff61
Ben Kelly <bk@ancilla.ca>
2014-11-23 23:40:17 -0500

    Update deploy.sh for 0.9.1
    A bunch of changes here. The obvious one is the change to the download
    targets and zip names, but the UI has also changed (it now always
    deploys the current branch). It also downloads a binary for LFS and
    *patches the DLL* to use the same lua DLL name as love2d itself, so that
    we don't end up with duplicate loads of liblua.

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,15 @@
 
 ZIP="zip -db -dc -q"
 UNZIP="unzip -q -o"
-BRANCHES="master dev"
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 SRC="$(git ls-files | egrep -v ^examples/)"
+LOVE="0.9.1"
 
 function main() {
   prepare
   [[ $@ ]] || set -- make-love make-win32 make-win64 make-osx deploy
   while [[ $1 ]]; do
-    for branch in $BRANCHES; do
-      $1 $branch
-    done
+    $1 $BRANCH
     shift
   done
   echo "Done."
@@ -25,10 +24,19 @@ function prepare() {
   echo "Preparing build environment"
   mkdir -p .build release
   rm -rf .build/emufun*
-  for arch in macosx-ub win-x86 win-x64; do
-    rm -rf ".build/love-0.8.0-$arch"
-    [[ -f ".build/love-0.8.0-$arch.zip" ]] || wget -q -P .build "https://bitbucket.org/rude/love/downloads/love-0.8.0-$arch.zip"
+  for arch in macosx-x64 win32 win64; do
+    rm -rf ".build/love-$LOVE-$arch"
+    [[ -f ".build/love-$LOVE-$arch.zip" ]] || {
+      echo "Downloading love-$LOVE-$arch"
+      wget -nv -P .build "https://bitbucket.org/rude/love/downloads/love-$LOVE-$arch.zip"
+    }
   done
+  [[ -f ".build/lfs.dll" ]] || {
+    echo "Downloading lfs.dll and patching with correct DLL name"
+    wget -nv -c -nc -P .build "http://files.luaforge.net/releases/luafilesystem/luafilesystem/luafilesystem-1.4.2/luafilesystem-1.4.2-win32-lua51.zip"
+    unzip -j -d .build .build/luafilesystem-1.4.2-win32-lua51.zip
+    dd if=<(printf 'lua51.dll\x00') of=.build/lfs.dll seek=9564 bs=1 conv=notrunc
+  }
 }
 
 function make-love() {
@@ -36,11 +44,12 @@ function make-love() {
   local name="release/emufun-$1.love"
   rm -f "$name"
   git archive --format=zip --output="$name" "$1"
+  $ZIP "$name" util/*
 }
 
 function make-osx() {
   echo "Making emufun-osx-$1.zip"
-  ${UNZIP} -d .build .build/love-0.8.0-macosx-ub.zip
+  ${UNZIP} -d .build .build/love-$LOVE-macosx-x64.zip
   mv .build/love.app .build/emufun.app
   cp "release/emufun-$1.love" .build/emufun.app/Contents/Resources/
   cp Info.plist .build/emufun.app/Contents/
@@ -49,29 +58,32 @@ function make-osx() {
 }
 
 function make-win() {
-  ${UNZIP} -d .build ".build/love-0.8.0-win-$2.zip"
-  mv ".build/love-0.8.0-win-$2" .build/emufun
-  cat "release/emufun-$1.love" .build/emufun/love.exe > .build/emufun/emufun.exe
+  ${UNZIP} -d .build ".build/love-$LOVE-$2.zip"
+  mv ".build/love-$LOVE-$2" .build/emufun
+  cp .build/lfs.dll .build/emufun/
+  cat .build/emufun/love.exe "release/emufun-$1.love" > .build/emufun/emufun.exe
   rm .build/emufun/love.exe
+  chmod a+x .build/emufun/*.{exe,dll}
   (cd .build && ${ZIP} -r emufun.zip emufun/)
   rm -rf .build/emufun
 }
 
 function make-win32() {
   echo "Making emufun-win32-$1.zip"
-  make-win $1 x86
+  make-win $1 win32
   mv .build/emufun.zip "release/emufun-win32-$1.zip"
 }
 
-function make-win64() {
-  echo "Making emufun-win64-$1.zip"
-  make-win $1 x64
-  mv .build/emufun.zip "release/emufun-win64-$1.zip"
-}
+# We don't have a 64-bit binary for lfs.dll. This is disabled until we do.
+# function make-win64() {
+#   echo "Making emufun-win64-$1.zip"
+#   make-win $1 win64
+#   mv .build/emufun.zip "release/emufun-win64-$1.zip"
+# }
 
 function deploy() {
-  echo "Deploying emufun-$1.love to orias"
-  cp release/*.love /orias/media/EmuFun/
+  echo "Deploying binaries to orias"
+  cp -uv release/* /orias/media/EmuFun/
 }
 
 main "$@"
