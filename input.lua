725c963a308e5aaa2c78b3284ac92b3822d89ebc
Ben Kelly <bk@ancilla.ca>
2014-11-25 21:39:57 -0500

    Use love2d 0.9.x gamepad API rather than joystick API.

--- input.lua
+++ input.lua
@@ -85,7 +85,7 @@ function input.setRepeat(event, delay, period)
 end
 
 -- love2d callback function for keyboard events. Pressing 'a' will trigger event
--- key_a() or, if that doesn't exist, key_any('a')
+-- key_a.
 function love.keypressed(name, num)
     if input.keyboard_enabled then
         return input.event("key_"..name)
@@ -98,92 +98,19 @@ function love.keyreleased(name, num)
     end
 end
 
--- same as above, but for joysticks. Joystick events are parameterized by both
--- stick and button; button 3 on stick 0 shows up as joy_0_button_3.
-function love.joystickpressed(j, b)
+-- The same, but for gamepads. Note that each event triggers "pad_X_Y", but if
+-- there is no event handler, falls through to "pad_*_Y", so that you can bind
+-- things to (e.g.) "the A button on any connected gamepad".
+function love.gamepadpressed(pad, button)
     if input.controller_enabled then
-        return input.event("joy_"..j:getID().."_button_"..b, j, "button", b)
-            or input.event("joy_*_button_*", j, "button", b)
+        return input.event("pad_"..pad:getID().."_"..button)
+            or input.event("pad_*_"..button)
     end
 end
 
-function love.joystickreleased(j, b)
+function love.gamepadreleased(pad, button)
     if input.controller_enabled then
-        return input.event("!joy_"..j:getID().."_button_"..b, j, "button", b)
-            or input.event("!joy_*_button_*", j, "button", b)
-    end
-end
-
--- joystick axis handling. At initialization, read the state of every axis on
--- every joystick and record them. We emit events only if the state of an
--- axis changes between frames, so that someone holding a stick up doesn't
--- trigger (say) joy_0_axis_2_up every single frame.
-local function readJoystick(j)
-    local function dir(val)
-        if val > 0.95 then
-            return "up"
-        elseif val < -0.95 then
-            return "down"
-        else
-            return "center"
-        end
-    end
-
-    local axes = { hats = {}; j:getAxes() }
-    for h=1,j:getHatCount() do
-        axes.hats[h] = j:getHat(h)
-    end
-    for a=1,#axes do
-        axes[a] = dir(axes[a])
-    end
-
-    return axes
-end
-
-local joysticks = {}
-for _,stick in ipairs(love.joystick.getJoysticks()) do
-    joysticks[stick:getID()] = readJoystick(stick)
-end
-
--- Every frame, scan joystick axes, compare them to the previous frame, and
--- emit events for any that have changed.
-local _update = love.update
-function love.update(dt)
-    _update(dt)
-
-    if not input.controller_enabled then
-        return
-    end
-
-    -- scan joysticks
-    for _,stick in ipairs(love.joystick.getJoysticks()) do
-        local j = stick:getID()
-        axes = joysticks[j]
-        new_axes = readJoystick(stick)
-
-        -- check hats
-        for h,ndir in ipairs(new_axes.hats) do
-            -- hat has moved since last time
-            local dir = axes.hats[h]
-            if ndir ~= dir then
-                if not input.event("!joy_"..j.."_hat_"..h.."_"..dir, j, "hat", h, dir) then
-                    input.event("!joy_*_hat_*_"..dir, j, "hat", h, dir)
-                end
-                if not input.event("joy_"..j.."_hat_"..h.."_"..ndir, j, "hat", h, ndir) then
-                    input.event("joy_*_hat_*_"..ndir, j, "hat", h, ndir)
-                end
-            end
-        end
-
-        -- check sticks
-        for a,ndir in ipairs(new_axes) do
-            local dir = axes[a]
-            if ndir ~= dir then
-                input.event("!joy_"..j.."_axis_"..a.."_"..dir, j, "axis", a, dir)
-                input.event("joy_"..j.."_axis_"..a.."_"..ndir, j, "axis", a, ndir)
-            end
-        end
-
-        joysticks[stick:getID()] = new_axes
+        return input.event("!pad_"..pad:getID().."_"..button)
+            or input.event("!pad_*_"..button)
     end
 end
